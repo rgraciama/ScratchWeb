ab502cda5562dd71c3a6fce36b941c835901d5b3
rgraciama <r.gracia.maiques@gmail.com>
2018-12-28 11:39:03 +0100

    Agregado sistema que auto detecta los elementos deleted

--- Xvideos/xvideos_deleteRemovedFavouriteVideos.js
+++ Xvideos/xvideos_deleteRemovedFavouriteVideos.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         XVideos Delete Removed Videos
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://xhamster.com/my/favorites/videos*
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+waitForKeyElements (".reset-cursor", removeRemovedVideo);
 
-
+function removeRemovedVideo() {
     if ($('.thumb-container > .status').parent().children('.info-line-top').length != 0) {
          $('.thumb-container > .status').parent().children('.info-line-top').children('.remove').click();
 
@@ -23,8 +23,5 @@
                         location.reload();
                     },500);
         }, 500);
-
-
-
     }
-})();
+}
