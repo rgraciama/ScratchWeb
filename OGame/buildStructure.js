d99a2c1a948a3762cdeb2c92694cd1c321ed9bc1
rgraciama <r.gracia.maiques@gmail.com>
2019-02-01 21:54:46 +0100

    Ogame bot

---
+++ OGame/buildStructure.js
@@ -0,0 +1,32 @@
+function buildAnyStructureExceptOneByName(structureName) {
+    var max = $('.fastBuild').length;
+    if (max != 0) {
+        var pos = 0;
+        var isStructure = true;
+        while (isStructure && (pos < max)) {
+            isStructure = $($('.fastBuild').get(pos)).attr("title").includes(structureName);
+            pos++;
+        }
+        if (!isStructure) $('.fastBuild').get(pos).click();
+    }
+}
+
+function buildStructureByName(structureName) {
+    var max = $('.fastBuild').length;
+    if (max != 0) {
+        var pos = 0;
+        var isStructure = false;
+        while (!isStructure && (pos < max)) {
+            isStructure = $($('.fastBuild').get(pos)).attr("title").includes(structureName);
+            pos++;
+        }
+        if (isStructure) $('.fastBuild').get(pos).click();
+    }
+}
+
+function buildFirstStructure() {
+    var max = $('.fastBuild').length;
+    if (max != 0) {
+        $('.fastBuild').get(0).click();
+    }
+}
