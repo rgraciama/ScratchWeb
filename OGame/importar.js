08a43d732b516a56c22ff212ff5aeb82491728e4
rgraciama <r.gracia.maiques@gmail.com>
2019-02-03 15:12:28 +0100

    Arreglando cosas

--- OGame/importar.js
+++ OGame/importar.js
@@ -5,15 +5,13 @@ function importarScript(nombre, callback) {
     document.querySelector("head").appendChild(s);
 }
 
-var headUrl = "https://cdn.jsdelivr.net/gh/rgraciama/ScratchWeb@latest/OGame/"
+var headUrl = "https://cdn.jsdelivr.net/gh/rgraciama/ScratchWeb@latest/OGame/";
 /* Entities & Functions */
 importarScript(headUrl & "entities/Target.js");
-
 /* Libraries */
 importarScript(headUrl & "libs/waitForKeyElements.js");
-
 /* utils */
 importarScript(headUrl & "util/buildStructure.js");
 importarScript(headUrl & "util/dashboard.js");
-
+/* main*/
 importarScript(headUrl & "_main.js");
