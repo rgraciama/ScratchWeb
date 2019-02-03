a490a9e32a8d4f84ed4e52af64f2f039fdea121b
rgraciama <r.gracia.maiques@gmail.com>
2019-02-03 10:31:56 +0100

    last version

--- OGame/importar.js
+++ OGame/importar.js
@@ -5,13 +5,13 @@ function importarScript(nombre, callback) {
     document.querySelector("head").appendChild(s);
 }
 /* Entities & Functions */
-importarScript("https://cdn.jsdelivr.net/gh/rgraciama/ScratchWeb/OGame/entities/Target.js");
+importarScript("https://cdn.jsdelivr.net/gh/rgraciama/ScratchWeb@latest/OGame/entities/Target.js");
 
 /* Libraries */
-importarScript("https://cdn.jsdelivr.net/gh/rgraciama/ScratchWeb/OGame/libs/waitForKeyElements.js");
+importarScript("https://cdn.jsdelivr.net/gh/rgraciama/ScratchWeb@latest/OGame/libs/waitForKeyElements.js");
 
 /* utils */
-importarScript("https://cdn.jsdelivr.net/gh/rgraciama/ScratchWeb/OGame/util/buildStructure.js");
-importarScript("https://cdn.jsdelivr.net/gh/rgraciama/ScratchWeb/OGame/util/dashboard.js");
+importarScript("https://cdn.jsdelivr.net/gh/rgraciama/ScratchWeb@latest/OGame/util/buildStructure.js");
+importarScript("https://cdn.jsdelivr.net/gh/rgraciama/ScratchWeb@latest/OGame/util/dashboard.js");
 
-importarScript("https://cdn.jsdelivr.net/gh/rgraciama/ScratchWeb/OGame/_main.js");
+importarScript("https://cdn.jsdelivr.net/gh/rgraciama/ScratchWeb@latest/OGame/_main.js");
