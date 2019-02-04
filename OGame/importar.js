76aad36535df52633100af52fac9ffd5f6c27918
rgraciama <r.gracia.maiques@gmail.com>
2019-02-04 21:03:41 +0100

    venga loco

---
+++ OGame/importar.js
@@ -0,0 +1,17 @@
+function importarScript(nombre, callback) {
+    var s = document.createElement("script");
+    s.onload = callback;
+    s.src = nombre;
+    document.querySelector("head").appendChild(s);
+}
+
+var headUrl = "https://cdn.jsdelivr.net/gh/rgraciama/ScratchWeb@master/OGame/";
+/* Entities & Functions */
+importarScript(headUrl + "entities/Target.js");
+/* Libraries */
+importarScript(headUrl + "libs/waitForKeyElements.js");
+/* utils */
+importarScript(headUrl + "util/buildStructure.js");
+importarScript(headUrl + "util/dashboard.js");
+/* main*/
+importarScript(headUrl + "_main.js");
--- OGame/script.js
+++ OGame/script.js
@@ -1,14 +1,10 @@
 // ==UserScript==
 // @name         OGame Auto build
 // @namespace    http://tampermonkey.net/
-// @version      0.11
+// @version      0.7
 // @description  try to take over the world!
 // @author       You
 // @match        https://s158-es.ogame.gameforge.com/game/index.php?page=*
-// @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/OGame/util/buildStructure.js
-// @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/OGame/util/dashboard.js
-// @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/OGame/libs/waitForKeyElements.js
-// @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/OGame/entities/Target.js
-// @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/OGame/_main.js
+// @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/OGame/importar.js
 // @grant        none
 // ==/UserScript==
