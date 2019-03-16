c23c0251332b6ee587c3c955907b57029eef9149
rgraciama <r.gracia.maiques@gmail.com>
2019-03-16 14:42:29 +0100

    insert a small script

--- Grepolis/script.js
+++ Grepolis/script.js
@@ -1,11 +1,10 @@
 // ==UserScript==
-// @name         Set Islands Labels Names
+// @name         Set Islands Labels Names2
 // @namespace    http://tampermonkey.net/
 // @version      0.7
 // @description  try to take over the world!
 // @author       You
-// @include      http://*.grepolis.com/game/*
-// @include      https://*.grepolis.com/game/*
-// @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/OGame/importar.js
+// @match 	 https://es78.grepolis.com/*
+// @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/Grepolis/islandsLabelsNames.js
 // @grant        none
 // ==/UserScript==
