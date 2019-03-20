ff09ef8e1009040cc6062dde27aba065abc4b670
rgraciama <r.gracia.maiques@gmail.com>
2019-03-21 00:33:09 +0100

    Bals

--- Grepolis/getMunecas_v013.js
+++ Grepolis/getMunecas_v013.js
@@ -8,8 +8,8 @@ function importarScript(nombre, callback) {
 var headUrl = "https://cdn.jsdelivr.net/gh/rgraciama/ScratchWeb/Grepolis/";
 
 importarScript("https://www.gstatic.com/firebasejs/5.9.0/firebase.js");
-importarScript(headUrl + "session/session_v007.js");
 importarScript(headUrl + "dbFirebase/dbFirebase_v002.js");
+importarScript(headUrl + "session/session_v007.js");
 importarScript(headUrl + "islands/islands_v006.js");
 importarScript(headUrl + "_main_v004.js");
 
