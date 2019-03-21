e40fe1df68b2584fa7d5487f1eb153035be2fd40
rgraciama <r.gracia.maiques@gmail.com>
2019-03-21 20:48:13 +0100

    Modificar el tema

--- Grepolis/islands/islands_v009.js
+++ Grepolis/islands/islands_v009.js
@@ -1,6 +1,11 @@
 var addOnClick = function() {
     if (!$("#input-island-label").is(":visible")) {
-        $('span.sea_coords').attr('onclick', 'addInputBox()');
+        $(".island_info").append("<input type='text' id='input-island-label'>");
+        var islandKey = $('.islandinfo_coords').text().match(/([.0-9]*\d\/[.0-9]*\d)/g);
+        islandKey = islandKey.toString().replace("/", "_");
+        
+        var islandName = $('#islandCity_'+islandKey).text();
+        $('#input-island-label').val(islandName);
     }
 }
 
--- Grepolis/script.js
+++ Grepolis/script.js
@@ -8,8 +8,7 @@
 // @require      https://www.gstatic.com/firebasejs/5.9.0/firebase.js
 // @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/Grepolis/dbFirebase/dbFirebase_v002.js
 // @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/Grepolis/session/session_v007.js
-// @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/Grepolis/islands/islands_v007.js
+// @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/Grepolis/islands/islands_v009.js
 // @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/Grepolis/_main_v004.js
-// @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/Grepolis/importLibs_v004.js
 // @grant        none
 // ==/UserScript==
