2f7d0d5078eb52891841b80544ad9aa41df1f18a
rgraciama <r.gracia.maiques@gmail.com>
2019-03-22 20:28:08 +0100

    islands

--- Grepolis/islands/islands_v012.js
+++ Grepolis/islands/islands_v012.js
@@ -19,22 +19,33 @@ var addOnClick = function() {
 
 function setMapViewNamesLabels(dbIslands) {
     //Islas Mapa
-    Object.keys(dbIslands).forEach(function(key) {
+    for (var [key, value] of islands) {
+        if (!$("span#island_"+value).is(":visible")) {
+            $('#mini_i'+value).append("<span id='island_"+value+"' class='labels-islands' style='position:absolute;left:20%;top:5px;z-index: 100;'>"+key+"</span>");
+        }
+    }
+
+    /*Object.keys(dbIslands).forEach(function(key) {
         //console.log('Key : ' + key + ', Value : ' + dbIslands[key]);
         if (!$("span#island_"+key).is(":visible")) {
             $('#mini_i'+key).append("<span id='island_"+key+"' class='labels-islands' style='position:absolute;left:20%;top:5px;z-index: 100;'>"+dbIslands[key]+"</span>");
         }
-    });
+    });*/
 
 }
 
 function setCityViewNamesLabels(dbIslands) {
     //Islas Mapa
-    Object.keys(dbIslands).forEach(function(key) {
+    for (var [key, value] of islands) {
+        if (!$("span#islandCity_"+value).is(":visible")) {
+            $('#island_'+value).append("<span id='islandCity_"+value+"' class='labels-islandsCity' style='position:absolute;left:20%;top:-20px;z-index: 900;'>"+key+"</span>");
+        }
+    }
+    /*Object.keys(dbIslands).forEach(function(key) {
         if (!$("span#islandCity_"+key).is(":visible")) {
             $('#island_'+key).append("<span id='islandCity_"+key+"' class='labels-islandsCity' style='font-weight: bold;position:absolute;left:20%;top:-20px;z-index: 900;'>"+dbIslands[key]+"</span>");
         }
-    });
+    });*/
 }
 
 function writeIslandName() {
