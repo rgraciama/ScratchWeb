4774720eba6fd1ab80c925cabf2839863606cb11
rgraciama <r.gracia.maiques@gmail.com>
2019-09-17 10:48:58 +0200

    First line in firebase

--- platzi-exams/exams/exam.js
+++ platzi-exams/exams/exam.js
@@ -4,7 +4,8 @@ function setExam() {
     // ==> Después el valor sea un map dónde la pregunta sea la key.
     // ====> El valor sea un map dónde la respuesta sea la key y si es cierto o falso el valor.
 
-    var examKey = window.location.href;
+    var examKeyStr = window.location.href;
+    var examKey = examKeyStr.replace(".", "_");
     var questionKey = 'todo';
     var answers = [{a: 1},{a: 2},{a: 3}];
     //TODO hacer que answer tengan la frase de la pregunta y valor en un map.
