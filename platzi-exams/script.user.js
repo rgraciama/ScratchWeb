49e7817f77104b9d127eec0c7b8935ff050c634c
rgraciama <r.gracia.maiques@gmail.com>
2019-09-18 17:35:47 +0200

    Add button results

--- platzi-exams/script.user.js
+++ platzi-exams/script.user.js
@@ -1,13 +1,12 @@
 // ==UserScript==
-// @name         ¡Muñecas de Famosa!
+// @name         Platzi Exams
 // @namespace    http://tampermonkey.net/
 // @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @match 	     https://es78.grepolis.com/*
+// @match 	     https://platzi.com/clases/examen/*
 // @require      https://www.gstatic.com/firebasejs/5.9.0/firebase.js
 // @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/platzi-exams/dbFirebase/dbFirebase.js
-// @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/platzi-exams/session/session.js
 // @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/platzi-exams/exams/exam.js
 // @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/platzi-exams/_main.js
 // @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/platzi-exams/importLibs.js
