478e6b07e26155d6140e781d1f4d5e4e29e9e0b4
rgraciama <r.gracia.maiques@gmail.com>
2019-09-19 14:04:43 +0200

    actu jsdelivr

--- platzi-exams/script.user.js
+++ platzi-exams/script.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Platzi Exams
 // @namespace    http://tampermonkey.net/
-// @version      1.12
+// @version      1.13
 // @description  try to take over the world!
 // @author       You
 // @match 	     https://platzi.com/clases/examen/*
 // @match 	     https://platzi.com/clases/exams/*
 // @require      https://www.gstatic.com/firebasejs/5.9.0/firebase.js
+// @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/platzi-exams/dbFirebase/dbFirebase.js
 // @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/platzi-exams/importLibs.js
 // @require      https://raw.githubusercontent.com/rgraciama/ScratchWeb/master/platzi-exams/_main.js
 // @grant        none
