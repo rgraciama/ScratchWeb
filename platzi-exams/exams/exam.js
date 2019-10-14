18233c04c21617fc8b485392a01866e10f0639ea
rgraciama <r.gracia.maiques@gmail.com>
2019-10-14 19:15:47 +0200

    Esto es la repanocha.
    He removido los TODO's y ahora estoy a la expectativa de nuevos bugs :D.

--- platzi-exams/exams/exam.js
+++ platzi-exams/exams/exam.js
@@ -6,13 +6,6 @@ examsRef.on('value', function (snapshot) {
     dbExams = snapshot.val();
 });
 
-function iniDbExams() {
-    examsRef = dbPlatzi.ref("/exams");
-    dbPlatzi.ref("/exams").on('value', function (snapshot) {
-        dbExams = snapshot.val();
-    });
-}
-
 function setExam() {
     // la idea es un map donde la key sea el examen.
     // ==> Después el valor sea un map dónde la pregunta sea la key.
@@ -36,70 +29,53 @@ function setExam() {
         writeAnswers(examKey, questionKey);
     }
     $('#write-question').val("");
-}
 
-function writeAnswers(examKey, questionKey) {
 
-    $(".Answer-content").each(function (index, answerElement) {
-        var answer = answerElement.textContent.trim();
-        answer = parseValueToSaveInFirebase(answer);
-        if ($('.Answer')[index].className.includes("is-selected")) {
+    function writeAnswers(examKey, questionKey) {
 
-            writeAnswerData(examKey, questionKey, answer, "*");
+        $(".Answer-content").each(function (index, answerElement) {
+            var answer = answerElement.textContent.trim();
+            answer = parseValueToSaveInFirebase(answer);
+            if ($('.Answer')[index].className.includes("is-selected")) {
 
-        } else {
+                writeAnswerData(examKey, questionKey, answer, "*");
 
-            writeAnswerData(examKey, questionKey, answer, "-");
+            } else {
 
-        }
-    });
-}
+                writeAnswerData(examKey, questionKey, answer, "-");
 
-function writeAnswersOnlySelected(examKey, questionKey) {
-    $(".Answer-content").each(function (index, answerElement) {
-        var answer = answerElement.textContent.trim();
-        answer = parseValueToSaveInFirebase(answer);
-        if ($('.Answer')[index].className.includes("is-selected")) {
-            writeAnswerData(examKey, questionKey, answer, "*");
-        }
-    });
-}
-
-function printAnswers(examKey, questionKey) {
-
-    //TODO esperar a la nueva pregunta para pintarla
-    $(".Answer-content").each(function (index, answerElement) {
-        var answer = answerElement.textContent.trim();
-        answer = parseValueToSaveInFirebase(answer);
-        try {
-            if (dbExams[examKey][questionKey][answer] === "F") {
-                $(this).css("background-color", "#fcafa4") //red
-            } else if (dbExams[examKey][questionKey][answer] === "T") {
-                $(this).css("background-color", "#00ff8c"); //green
             }
-        } catch (e) {
-            Console.log("No se encuentra: " + examKey + ", " + questionKey + ", " + answer);
-        }
-
-    });
-}
-
-//write exam
-function writeAnswerData(examKey, questionKey, answer, value) {
-    try {
-        dbPlatzi.ref('/exams/' + examKey).child(questionKey).child(answer).set(value);
-    } catch (e) {
-        console.log("No se puede guardar, error: " + e);
-        console.log("exam: " + examKey);
-        console.log("question: " + questionKey);
+        });
     }
+
+    function writeAnswersOnlySelected(examKey, questionKey) {
+        $(".Answer-content").each(function (index, answerElement) {
+            var answer = answerElement.textContent.trim();
+            answer = parseValueToSaveInFirebase(answer);
+            if ($('.Answer')[index].className.includes("is-selected")) {
+                writeAnswerData(examKey, questionKey, answer, "*");
+            }
+        });
+    }
+
+    function resetExamQuestions(examKey, questionKey) {
+        for (var clave in dbExams[examKey][question]) {
+            writeAnswerData(examKey, questionKey, answer, "-");
+        }
+    }
+
+    function checkIfExamHasAstersisk(exam, questionKey) {
+        for (var clave in dbExams[examKey][question]) {
+            if (dbExams[examKey][question][clave] === '*') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
 
-function parseValueToSaveInFirebase(str) {
-    return str.replace(/[\(\)\/.,$\]\[]/g, "_");
-}
-
-/*** Set Results Exams **/
 function setResults() {
     //check results and give the results
     var examKey = $('.ResultInfo-careerName')[0].innerText;
@@ -120,79 +96,60 @@ function setResults() {
     });
 
     $('#write-result').val("");
-}
 
-function modifyAnswers(examKey, question, correct) {
-    try {
-        if (dbExams[examKey][question] !== null) {
-            if (checkQuestionHasCorrectAnswer(examKey, question) && correct) {
-                writeResultsOnAnswer(examKey, question);
-            } else if (!correct) {
-                writeResultsOnAnswerNoCorrect(examKey, question);
-            } else {
-                console.log("Condición extraña, es posible que hayas acertado, pero el programa no sepa que respuesta pusiste");
-                console.log("ExamKey: " + examKey);
-                console.log("Question: " + question);
+    function modifyAnswers(examKey, question, correct) {
+        try {
+            if (dbExams[examKey][question] !== null) {
+                if (checkQuestionHasCorrectAnswer(examKey, question) && correct) {
+                    writeResultsOnAnswer(examKey, question);
+                } else if (!correct) {
+                    writeResultsOnAnswerNoCorrect(examKey, question);
+                } else {
+                    console.log("Condición extraña, es posible que hayas acertado, pero el programa no sepa que respuesta pusiste");
+                    console.log("ExamKey: " + examKey);
+                    console.log("Question: " + question);
+                }
+
+
             }
-
-
-        }
-    } catch (e) {
-        console.log("Error: " + e);
-        console.log("ExamKey: " + examKey);
-        console.log("Question: " + question);
-    }
-}
-
-
-function checkQuestionHasCorrectAnswer(examKey, question) {
-    for (var clave in dbExams[examKey][question]) {
-        if (dbExams[examKey][question][clave] === '*') return true;
-    }
-    return false;
-}
-
-function writeResultsOnAnswer(examKey, question) {
-    for (var clave in dbExams[examKey][question]) {
-        if (dbExams[examKey][question][clave] === '*') {
-            writeAnswerData(examKey, question, clave, 'T');
-        } else if (dbExams[examKey][question][clave] === '-') {
-            writeAnswerData(examKey, question, clave, 'F');
+        } catch (e) {
+            console.log("Error: " + e);
+            console.log("ExamKey: " + examKey);
+            console.log("Question: " + question);
         }
     }
 
-}
-
-function writeResultsOnAnswerNoCorrect(examKey, question) {
-    for (var clave in dbExams[examKey][question]) {
-        if (dbExams[examKey][question][clave] === '*') {
-            writeAnswerData(examKey, question, clave, 'F');
+    function writeResultsOnAnswerNoCorrect(examKey, question) {
+        for (var clave in dbExams[examKey][question]) {
+            if (dbExams[examKey][question][clave] === '*') {
+                writeAnswerData(examKey, question, clave, 'F');
+            }
         }
+
     }
 
-}
-
-function checkIfExamHasAstersisk(exam, questionKey) {
-    for (var clave in dbExams[examKey][question]) {
-        if (dbExams[examKey][question][clave] === '*') {
-            return true;
+    function writeResultsOnAnswer(examKey, question) {
+        for (var clave in dbExams[examKey][question]) {
+            if (dbExams[examKey][question][clave] === '*') {
+                writeAnswerData(examKey, question, clave, 'T');
+            } else if (dbExams[examKey][question][clave] === '-') {
+                writeAnswerData(examKey, question, clave, 'F');
+            }
         }
-    }
-    return false;
-}
 
-function resetExamQuestions(examKey, questionKey) {
-    for (var clave in dbExams[examKey][question]) {
-        writeAnswerData(examKey, questionKey, answer, "-");
+    }
+
+    function checkQuestionHasCorrectAnswer(examKey, question) {
+        for (var clave in dbExams[examKey][question]) {
+            if (dbExams[examKey][question][clave] === '*') return true;
+        }
+        return false;
     }
 }
 
 var prevQuestion;
-function getQuestion() {
 
-    if (dbExams === undefined) {
-        iniDbExams();
-    }
+function getQuestion() {
 
     var examKey = $('.Course')[0].innerText;
     examKey = parseValueToSaveInFirebase(examKey);
@@ -211,6 +168,39 @@ function getQuestion() {
         }
         prevQuestion = questionKey;
     }
+
+    function printAnswers(examKey, questionKey) {
+
+        //TODO esperar a la nueva pregunta para pintarla
+        $(".Answer-content").each(function (index, answerElement) {
+            var answer = answerElement.textContent.trim();
+            answer = parseValueToSaveInFirebase(answer);
+            try {
+                if (dbExams[examKey][questionKey][answer] === "F") {
+                    $(this).css("background-color", "#fcafa4") //red
+                } else if (dbExams[examKey][questionKey][answer] === "T") {
+                    $(this).css("background-color", "#00ff8c"); //green
+                }
+            } catch (e) {
+                Console.log("No se encuentra: " + examKey + ", " + questionKey + ", " + answer);
+            }
+
+        });
+    }
+
 }
 
-//Añado comment para ver si actualiza el jsdelivr
+//write exam
+function writeAnswerData(examKey, questionKey, answer, value) {
+    try {
+        dbPlatzi.ref('/exams/' + examKey).child(questionKey).child(answer).set(value);
+    } catch (e) {
+        console.log("No se puede guardar, error: " + e);
+        console.log("exam: " + examKey);
+        console.log("question: " + questionKey);
+    }
+}
+
+function parseValueToSaveInFirebase(str) {
+    return str.replace(/[\(\)\/.,$\]\[]/g, "_");
+}
